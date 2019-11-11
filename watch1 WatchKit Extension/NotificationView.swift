fdf77d927aa94c121e28eccbc633ad5e5506f14c
Mark Booth <mark@markandpol.com>
2019-11-11 09:07:15 +0000

    Initial Commit

---
+++ watch1 WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  watch1 WatchKit Extension
+//
+//  Created by Mark Booth on 11/11/2019.
+//  Copyright © 2019 Mark Booth. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
